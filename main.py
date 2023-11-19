5974117f4f1084d37b16b3038cf057054258708a
pann <i@pann.me>
2023-11-19 18:02:40 +0900

    comment out code for dev

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from fastapi import FastAPI
 from typing import Optional
 from db import Database
-from fastapi.middleware.cors import CORSMiddleware
+
+# from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-origins = [
-	"http://localhost:3000"
-]
 
-app.add_middleware(
-	CORSMiddleware,
-	allow_origins=origins,
-	allow_credentials=True,
-	allow_methods=["*"],
-	allow_headers=["*"],
-)
+# origins = [
+# 	"http://localhost:3000"
+# ]
+#
+# app.add_middleware(
+# 	CORSMiddleware,
+# 	allow_origins=origins,
+# 	allow_credentials=True,
+# 	allow_methods=["*"],
+# 	allow_headers=["*"],
+# )
 
 
 @app.get("/query_books")
